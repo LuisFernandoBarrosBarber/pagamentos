df31fc4d7d19904233a9bcde72fa3822a5020a90
Luis Fernando de Barros <luis-barros@defensoria.rs.def.br>
2022-07-22 20:24:44 -0300

    add rota nova assinatura

--- src/main/java/com/barbearia/pagamentos/service/ContratoService.java
+++ src/main/java/com/barbearia/pagamentos/service/ContratoService.java
@@ -1,6 +1,5 @@
 package com.barbearia.pagamentos.service;
 
-import com.barbearia.pagamentos.model.Assinatura;
 import com.barbearia.pagamentos.model.Cliente;
 import com.barbearia.pagamentos.model.Contrato;
 import lombok.RequiredArgsConstructor;
@@ -18,8 +17,8 @@ public class ContratoService {
     @Transactional
     public Contrato novo(Contrato co) {
         Cliente cl = clienteService.novo(co.getNome(), co.getId(), co.getCpf());
-        Assinatura a = assinaturaService.assinar(cl, co);
-        co.setAssinaturaIdAsaas(a.getIdAsaas());
+       // Assinatura a = assinaturaService.assinar(cl, co);
+        //co.setAssinaturaIdAsaas(a.getIdAsaas());
         co.setClienteIdAsaas(cl.getIdAsaas());
         return co;
     }
