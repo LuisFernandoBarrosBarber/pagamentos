72807690e32897a8b62843004d1e69f7ada9b9ad
Luis Fernando de Barros <luibs91vendas@gmail.com>
2022-09-13 16:38:31 -0300

    atualiza tbm faturas vencidas

--- src/main/java/com/barbearia/pagamentos/repository/CobrancaRepository.java
+++ src/main/java/com/barbearia/pagamentos/repository/CobrancaRepository.java
@@ -4,6 +4,7 @@ import com.barbearia.pagamentos.dto.asaas.enumerator.StatusCobranca;
 import com.barbearia.pagamentos.entities.CobrancaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -13,7 +14,7 @@ public interface CobrancaRepository extends JpaRepository<CobrancaEntity, Long> 
 
     Stream<CobrancaEntity> getByIdAssinaturaAndAtivoIsTrueAndStatus(String a, StatusCobranca s);
 
-    Stream<CobrancaEntity> findByStatusAndAtivoIsTrue(StatusCobranca s);
+    Stream<CobrancaEntity> findByStatusInAndAtivoIsTrue(List<StatusCobranca> s);
 
     Optional<CobrancaEntity> findFirstByAtivoIsTrueAndIdAssinaturaAndStatusOrderByVencimentoEm(String idAssinatura,
             StatusCobranca status);
